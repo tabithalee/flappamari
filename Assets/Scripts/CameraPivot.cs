21fbe279c13215ccc70b947a5b48a2a1ed6ca05f
tabithalee <leetabithagemma@gmail.com>
2022-07-13 21:17:23 -0700

    rotating whole ballParent

--- Assets/Scripts/CameraPivot.cs
+++ Assets/Scripts/CameraPivot.cs
@@ -5,32 +5,15 @@ using UnityEngine;
 public class CameraPivot : MonoBehaviour
 {
     // The target marker.
-    public GameObject gameCamera;
-    public GameObject sphere;
+    public GameObject ballParent;
 
     // Angular speed in radians per sec.
-    public float angle = 90f;
+    public float angle = 45f;
 
     public IEnumerator PivotSphere()
-    //{
-    //    // Determine which direction to rotate towards
-    //    Vector3 targetDirection = Vector3.left;
-
-    //    // The step size is equal to speed times frame time.
-    //    float singleStep = speed * Time.deltaTime;
-
-    //    // Rotate the forward vector towards the target direction by one step
-    //    Vector3 newDirection = Vector3.RotateTowards(sphereParent.forward, targetDirection, singleStep, 0.0f);
-
-    //    // Draw a ray pointing at our target in
-    //    Debug.DrawRay(sphereParent.position, newDirection, Color.red);
-
-    //    // Calculate a rotation a step closer to the target and applies rotation to this object
-    //    sphereParent.rotation = Quaternion.LookRotation(newDirection);
-    //}
     {
-        gameCamera.transform.RotateAround(sphere.transform.position, Vector3.up, angle);
-        yield return new WaitForSeconds(5);
+        ballParent.transform.RotateAround(ballParent.transform.position, Vector3.up, angle);
+        yield return new WaitForSeconds(2);
         Debug.Log("finished pivot");
     }
 }
